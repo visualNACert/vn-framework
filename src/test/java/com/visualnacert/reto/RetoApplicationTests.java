227f2f83987ee8141f4f8c01a891326a3f732bf8
Javier Andres <jandres@visualnacert.com>
2024-05-09 09:20:24 +0200

    delete tests

--- src/test/java/com/visualnacert/reto/RetoApplicationTests.java
+++ src/test/java/com/visualnacert/reto/RetoApplicationTests.java
@@ -1,13 +1,40 @@
 package com.visualnacert.reto;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
-class RetoApplicationTests {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RetoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	private static String baseUrl = "http://localhost:";
 
 	@Test
-	void contextLoads() {
-	}
+	public void testConcurrentRequests() throws InterruptedException {
+		int numThreads = 10; // Number of concurrent threads
+		ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
 
+		for (int i = 0; i < numThreads; i++) {
+			executorService.execute(() -> {
+				RestTemplate restTemplate = new RestTemplate();
+				String response = restTemplate.getForObject(baseUrl + port + "/reto", String.class);
+				assertEquals("OK", response);
+			});
+		}
+
+		// Espera a que todas las solicitudes concurrentes terminen
+		executorService.shutdown();
+		executorService.awaitTermination(30, TimeUnit.SECONDS);
+	}
 }
